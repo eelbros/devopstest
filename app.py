f96d84c579b7142df7253cdd017d340eec8460db
Zameel <zameel.h@gmail.com>
2025-07-28 06:10:39 +0300

    Modified permissions on github

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask (__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello, KSA! I have missed you so much. This app is running on AAWS Fargate'
+	return 'Hello, KSA! I have missed you so much. This app is running on AWS Fargate'
 
 if __name__== '__main__':
 	app.run(host='0.0.0.0', port=80)
