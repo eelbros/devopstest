db074a99e8133f4682ada71cb24eb45431cbafdf
eelbros <zameel.h@gmail.com>
2025-07-28 07:56:01 +0300

    Fixed syntax in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask (__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello, KSA! I don't know where I would be without you. This app is running on AWS Fargate'
+	return 'Hello, KSA! I don\'t know where I would be without you. This app is running on AWS Fargate'
 
 if __name__== '__main__':
 	app.run(host='0.0.0.0', port=80)
